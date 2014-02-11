9aebc48e460edb738004539e85fdb58daeaca75a
Bogdan Tchaicka <perldev@mail.ru>
2014-02-11 08:26:14 +0200

    fix check hbase

--- include/prolog.hrl
+++ include/prolog.hrl
@@ -2,7 +2,7 @@
 %%default count of pool to the thrift server
 %% wheather using thrift
 -define(USE_THRIFT, 1).
--define(SIMPLE_HBASE_ASSERT, 1). %  common:check_source(TreeEts) ).
+-define(SIMPLE_HBASE_ASSERT,   common:check_source(TreeEts) ).
 
 %-define(THRIFT_RECONNECT_TIMEOUT,130000).
 %-define(THRIFT_MAX_RECONNECT_COUNT, 10).
